eaa7cd22aeb9080b4e81e3f362acfa6c6197f9f2
RKlott <proklott@gmail.com>
2026-02-06 11:59:33 +0100

    commencement de l'exo 6, vérifier la maintenabilité et la logique des classes

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,212 @@
+//Exo 5
+class heroClass {
+  DEFAULT_POWER_LEVEL = 50;
+  DEFAULT_STRENGTH_RATIO = 50;
+  DEFAULT_HEALTH = 500;
+  DEFAULT_NAME = "Unknown";
+
+  constructor() { //? Devrais-je laisser le constructeur vide ?
+    this.name = this.DEFAULT_NAME;
+    this.basicHealth = this.DEFAULT_HEALTH;
+
+    this.powerLevel = this.DEFAULT_POWER_LEVEL;
+    this.strengthRatio = this.DEFAULT_STRENGTH_RATIO;
+  }
+
+  //?Getters
+
+  getName() {
+    return this.name;
+  }
+
+  getBasicPowerLevel() {
+    return this.powerLevel;
+  }
+
+  getBasicHealth() {
+    return this.basicHealth;
+  }
+
+  getBasicStrengthRatio() {
+    return this.strengthRatio;
+  }
+
+  //?Setters pour les héros
+
+  setPowerLevel(arg) {
+    this.powerLevel = arg;
+    return;
+  }
+
+  setHealth(arg) {
+    this.basicHealth = this.basicHealth + arg;
+    return;
+  }
+
+  setStrengthRatio(arg) {
+    this.strengthRatio = this.strengthRatio + arg;
+    return;
+  }
+
+  rename(arg1, arg2) {
+    arg1 = arg2;
+    return;
+  }
+}
+
+class weaponClass extends heroClass {
+  constructor(holderName, weaponName, weaponDesc, powerLevel = this.getBasicPowerLevel()) {
+    this.holderName = holderName;
+    this.powerLevel = powerLevel;
+    this.weaponName = weaponName;
+    this.weaponDesc = weaponDesc;
+  }
+
+  //? Getters
+
+  getWeaponName() {
+    return this.weaponName;
+  }
+
+  getWeaponDesc() {
+    return this.weaponDesc;
+  }
+
+  getWeaponPowerLevel() {
+    return this.powerLevel;
+  }
+
+  getHolderName() {
+    return this.holderName;
+  }
+
+  //? Setters
+
+  renameThisWeapon(arg) {
+    this.weaponName = arg;
+  }
+
+  setWeaponDesc(arg) {
+    this.weaponDesc = arg;
+  }
+
+  setWeaponPowerLevel(arg) {
+    this.powerLevel = arg;
+  }
+
+  setHolderName(arg) {
+    this.holderName = arg;
+  }
+}
+
+class spellClass extends heroClass {
+  constructor(
+    holderName,
+    spellName,
+    spellDesc,
+    powerLevel = this.getBasicPowerLevel(),
+  ) {
+    this.holderName = holderName;
+    this.powerLevel = powerLevel;
+    this.spellName = spellName;
+    this.spellDesc = spellDesc;
+  }
+
+  //? Getters
+
+  getSpellName() {
+    return this.spellName;
+  }
+
+  getSpellDesc() {
+    return this.spellDesc;
+  }
+
+  getSpellPowerLevel() {
+    return this.powerLevel;
+  }
+
+  getHolderName() {
+    return this.holderName;
+  }
+  //? Setters
+
+  renameThisSpell(arg) {
+    this.spellName = arg;
+  }
+
+  setSpellDesc(arg) {
+    this.spellDesc = arg;
+  }
+
+  setSpellPowerLevel(arg) {
+    this.powerLevel = arg;
+  }
+
+  setHolderName(arg) {
+    this.holderName = arg;
+  }
+}
+
+///* combatClass
+class Warrior extends heroClass {
+  constructor(holderName, basicHealth, strengthRatio) {
+    super(holderName, basicHealth, strengthRatio);
+  }
+
+  warriorBonusHealth = 200;
+  warriorBonusStrengthRatio = 250;
+  title = "Guerrier";
+
+  getHeroName() {
+    return this.getName();
+  }
+
+  getHealth() {
+    return this.getBasicHealth() + this.warriorBonusHealth;
+  }
+
+  getStrengthRatio() {
+    return this.getBasicStrengthRatio() + this.warriorBonusStrengthRatio;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+}
+
+class Mage extends heroClass {
+  constructor(holderName, basicHealth) {
+    super(holderName, basicHealth);
+  }
+
+  strengthRatio = 50;
+}
+//////////////////////////////////////////////////////////////////////
+//Exo 4
+let tableauChiffres = [2, 4, 3, 8, 10];
+
+class arrayCalculator {
+  constructor(array) {
+    this.array = array;
+  }
+
+  calculate() {
+    let sum = 0;
+    for (let i = 0; i < this.array.length; i++) {
+      sum += this.array[i];
+    }
+
+    console.log("Total des elements du tableau de l'exo 4 : " + sum);
+  }
+}
+
+const nda = new arrayCalculator(tableauChiffres);
+
+console.log(nda.calculate());
+
+////////////////////////////////////////////////////////////////////
+
 let modalButton = document.getElementById("modalButton");
 let closeModal = document.getElementById("closeModal");
 let myModal = document.getElementById("myModal");
@@ -10,10 +219,12 @@ closeModal.addEventListener("click", () => {
   myModal.style.display = "none";
 });
 
-window.addEventListener("click", (e) => { //TODO: corriger le soucis ici, cette fonction empêche l'utilisation de la modale, sûrement le premier if statement qui fait défaut
+window.addEventListener("click", (e) => {
+  //TODO: corriger le soucis ici, cette fonction empêche l'utilisation de la modale, sûrement le premier if statement qui fait défaut
 
-  if (e.target !== myModal) { //! problème, le site analyse qu'il as un display flex depuis le clique du bouton, donc le display none est immédiat
-      myModal.style.display = "none";                             //? Checker si c'est suffisant
+  if (e.target === myModal) {
+    //! problème, le site analyse qu'il as un display flex depuis le clique du bouton, donc le display none est immédiat
+    myModal.style.display = "none"; //? Checker si c'est suffisant
   }
 });
 
@@ -28,7 +239,10 @@ function feuTricolore() {
   const delay = 1000;
 
   setTimeout(() => {
-    Array.from(feu).reverse().forEach((element, index) => {//ajout du reverse pour que ça commence au vert
+    Array.from(feu)
+      .reverse()
+      .forEach((element, index) => {
+        //ajout du reverse pour que ça commence au vert
         setTimeout(
           () => {
             element.classList.add("brightness");
